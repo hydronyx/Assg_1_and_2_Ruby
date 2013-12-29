ff28de50823a89edf63ed44eb091bd356a63cdb3
suyog-nerkar-webonise <suyog.nerkar@weboniselab.com>
2013-12-29 19:30:15 +0530

    commit 4 few more questions added

--- ruby_assignment_one.rb
+++ ruby_assignment_one.rb
@@ -1,5 +1,11 @@
+#Question 5 : Find occurrence of RUBY from string 1.
 
-# Question 8:
+str="RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag."
+puts " The count of the string RUBY are :  str.count('RUBY')"
+
+
+
+# Question 8: Capitalise string 1
 
 string_one= "RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code.The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag."
 => "RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag."
@@ -7,7 +13,9 @@ string_one= "RUBY parses a file by looking for <br/> one of the special tags tha
 => "RUBY PARSES A FILE BY LOOKING FOR <BR/> ONE OF THE SPECIAL TAGS THAT TELLS IT TO START INTERPRETING THE TEXT AS RUBY CODE. THE PARSER THEN EXECUTES ALL OF THE CODE IT FINDS UNTIL IT RUNS INTO A RUBY CLOSING <BR/> TAG."
 >  
 
-# Question 9 :
+
+
+# Question 9 : Combine string 1 & 2.
 
 > srting_one ="RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag."
 => "RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag."
@@ -21,7 +29,8 @@ string_one= "RUBY parses a file by looking for <br/> one of the special tags tha
 >  
 
 
-# Question 10 :
+
+# Question 10 : Print current date.
 
 > time = Time.new
 => 2013-12-29 08:41:04 UTC
@@ -30,7 +39,8 @@ string_one= "RUBY parses a file by looking for <br/> one of the special tags tha
 >  
 
 
-# Question No 12 :
+
+# Question No 12 : add  7 days in current date
 
 date=Time.now + (60 * 60 * 24 * 7)
 => 2014-01-05 11:41:02 UTC
@@ -39,7 +49,9 @@ date=Time.now + (60 * 60 * 24 * 7)
 > puts date.strftime("%d-%m-%y")
 => "05-01-14"
 
-# Question 17 :
+
+
+# Question 17 : Find the length of string 1 & 2.
 
 puts string_one
 => "RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. "
@@ -50,3 +62,24 @@ puts string_one
 => "RUBY does not require (or support) explicit type definition in variable declaration; a variable's type is determined by the context in which the variable is used."
 > string_two.length
 => 162
+
+
+
+#Question 18:  Compare two dates. (12-04-2010 & 12-05-2011). Calculate the days between these two dates.
+
+date_previous = Date.parse('2010-04-12')
+date_now = Date.parse('2011-05-12')
+how_many_days_have_passed = (date_now - date_previous).to_i
+=> 395
+							#runs on https://rubymonk.com/learning/books/1-ruby-primer/chapters/6-objects/lessons/35-introduction-to-objects
+
+	
+	
+# Question 19: Print date after 20 days from current date
+
+Time.now + (60 * 60 * 24 * 20)
+=> 2014-01-18 11:32:16 UTC
+> date= Time.now + (60 * 60 * 24 * 20)
+=> 2014-01-18 11:34:17 UTC
+> puts date.strftime("%d %b %y ")
+=> "18 Jan 14 "
