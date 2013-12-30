58ba4c42264d1044092d53b897b733812dc1eff2
webonise <webonise@webonise-desktop.(none)>
2013-12-30 16:56:38 +0530

    Assignment 2 2nd Push

--- ruby_assignment_two.rb
+++ ruby_assignment_two.rb
@@ -1,5 +1,28 @@
 # ASSIGNMENT NO 2 - RUBY BASICS QUESTION 1-4
 
+
+puts "QUESTION No 1 : Compute the sum of cubes for a given range a through b." \
+   + " Write a method called sum_of_cubes to accomplish this task."            \
+   + " Example Given range 1 to 3 the method should return 36."                \
+
+def sum_of_cubes(a,b)
+
+  sum=0;
+  (a..b).each{|x| sum+=(x**3)}
+  sum 
+end
+
+  puts sum_of_cubes(1,3)
+
+# OR
+
+def sum_of_cubes(a,b)
+  (a..b).inject(0) { |sum, n| sum=sum + (n**3) }
+end
+  puts sum_of_cubes(1,3)
+
+
+
 puts "QUESTION No 2 : Given an Array, return the elements that are present"   \
    + "exactly once in the array. You need to write a method called"           \ 
    + "non_duplicated_values to accomplish this task. "                        \
@@ -34,5 +57,8 @@ def palindrome?(sentence)
 end
 
   print palindrome?(sentence_one)
+
+
+
 
 
